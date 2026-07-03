c99a716ddcb800908b7fbf90ebbe69c2f77a036f
zepedaherrerab-droid <zepedaherrerab@gmail.com>
2026-07-02 23:31:11 -0400

    Megusta

---
+++ Megusta.js
@@ -0,0 +1,20 @@
+let likes1 = 0
+let likes2 = 0
+let likes3 = 0
+
+function darLike1(){
+    likes1++;
+    document.getElementById("span1").innerText = likes1 ;
+
+}
+
+function darLike2(){
+    likes2++;
+    document.getElementById("span2").innerText = likes2 ;
+
+}
+function darLike3(){
+    likes3++;
+    document.getElementById("span3").innerText = likes3 ;
+
+}
